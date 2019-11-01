a4ef173b89bb5fa86f86ba78ac81e80ae61e62a9
Vitor Recoaro <50752991+Vitorrecoaro@users.noreply.github.com>
2019-11-01 01:02:00 -0300

    Add files via upload

--- TF_Windows.c
+++ TF_Windows.c
@@ -4,7 +4,8 @@ Programa feito para o trabalho final da disciplina de Libras da UNIFEI, no ano d
 Feito por: Bruno Medri, Daniel Henrique, Caio Mello, Thiago Roberto, Ulisses Mira, Vitor Recoaro e Yerro Candido
 Data: 30/10/2019
 Objetivo: Programa que trará sinais em libras que o usuário deverá responder qual o significado do sinal mostrado.
-Edição: V1_Windows.
+Edição: V1.2_Windows.
+Adições: Modos de revisões e níveis de frases em libras.
 
 */
 
@@ -20,55 +21,73 @@ typedef struct sinais{
     char nome[100],link[10000];
 }no;
 
-void revisaoSinal(int n);
-void addFILE(no *n1);
-int qtdSinais();
+void revisaoSinal(int n,int rev);
+void addFILE(no *n1,int rev);
+int qtdSinais(int rev);
+void revisaodiaria();
 
 int main(){
     setlocale(LC_ALL,"UTF-8");
-    int op,test=0,vazio;
+    int op,test=0;
     char continuar,ok = 's';
     FILE *arq;
     arq = fopen("ListasSinais.txt","a+");
     
-    printf("-x-x-x- Menu -x-x-x-\n\n1- Revisao.\n2- Aprender novos sinais.\n0- Sair\n\n");
+    printf("-x-x-x- Menu -x-x-x-\n\n1- Revisao basica.\n2- Revisao avancada.\n3- Teste diario\n0- Sair\n\n");
     scanf(" %d",&op);
     system("cls");
     while(op!=0){
         if(op==1){
             srand(time(NULL));
-            int sinais=(qtdSinais()),n;
+            int sinais=(qtdSinais(1)),n;
             sinais++;
             n = (rand() % sinais);
             while((n%2)!=0 || n==0){
                 n = (rand()%sinais);
             }
-            fseek(arq,0,SEEK_END);
-            vazio = ftell(arq);
-            if(vazio == 0){
-                fclose(arq);
-            }
+            revisaoSinal(n,1);
+            printf("Mais um sinal [s/n]? ");
+            scanf(" %c",&continuar);
+            system("cls");
+            if(continuar=='s'){
+                test = 1;
+             }
             else{
-                revisaoSinal(n);
-                printf("Mais um sinal [s/n]? ");
-                while(getchar()!='\n');
-                scanf(" %c",&continuar);
-                system("cls");
-                if(continuar=='s'){
-                    test = 1;
-                }
-                else{
-                    test = 0;
-                }
+                test = 0;
             }
         }
         else if(op==2){
+            srand(time(NULL));
+            int sinais=(qtdSinais(2)),n;
+            sinais++;
+            n = (rand() % sinais);
+            while((n%2)!=0 || n==0){
+                n = (rand()%sinais);
+            }
+            revisaoSinal(n,2);
+            printf("Mais um sinal [s/n]? ");
+            scanf(" %c",&continuar);
+            system("cls");
+            if(continuar=='s'){
+                test = 1;
+             }
+            else{
+                test = 0;
+            }            
+        }
+        else if(op == 3){
+        }
+        else if(op==400){
             no n1;
+            int tipo;
+
+            printf("-x-x-x- Menu Arquivos -x-x-x-\n\n1- Sinais Basicos\n2- Sinais avancados\n\n");
+            scanf(" %d",&tipo);
             printf("Digite o nome do novo sinal e um link do video do sinal.\n");
             scanf(" %[^\n] %[^\n]",n1.nome,n1.link);
-            addFILE(&n1);
+            addFILE(&n1,tipo);
             printf("Voce aprendera o novo sinal.\n");
-            printf("Digite \"y\" para continuar\n");
+            printf("Aperte \"y\" para continuar\n");
             scanf(" %c",&ok);
             while(ok!='y'){
                 scanf(" %c",&ok);
@@ -76,22 +95,27 @@ int main(){
             system("cls");
         }
         if(test==0){
-            printf("-x-x-x- Menu -x-x-x-\n\n1- Revisao.\n2- Aprender novos sinais.\n0- Sair\n\n");
+            printf("-x-x-x- Menu -x-x-x-\n\n1- Revisao basica.\n2- Revisao avancada.\n3- Teste diario\n0- Sair\n\n");
             scanf(" %d",&op);
             system("cls");          
         }
         else{
-            op = 1;
+            op = op;
         }
     }
 }
 
-void revisaoSinal(int n){
+void revisaoSinal(int n,int rev){
     setlocale(LC_ALL,"UTF-8");
     int cont=0,lresp=n-2,p=0,erros; 
     char resp[100],frase[100],teste,link[10000],lose;
     FILE *arq;
-    arq = fopen("ListasSinais.txt","r");
+    if(rev == 1){
+        arq = fopen("ListasSinais.txt","r");
+    }
+    else if(rev == 2){
+        arq = fopen("ListasSinais2.txt","r");
+    }
     for(p=0;p<100;p++){
         frase[p]='\0';
     }
@@ -140,24 +164,33 @@ void revisaoSinal(int n){
     for(p=0;p<100;p++){
         frase[p]='\0';
     }
-    return;
 }
 
-void addFILE(no *n1){
+void addFILE(no *n1,int rev){
     setlocale(LC_ALL,"UTF-8");
     FILE *arq;
-    arq = fopen("ListasSinais.txt","a");
+    if(rev == 1){
+        arq = fopen("ListasSinais.txt","a");
+    }
+    else if (rev == 2){
+        arq = fopen("ListasSinais2.txt","a");
+    }
     fprintf(arq,"%s\n%s\n",n1->nome,n1->link);
     fclose(arq);
     return;
 }
 
-int qtdSinais(){
+int qtdSinais(int rev){
     setlocale(LC_ALL,"UTF-8");
     int cont=0;
     char test;
     FILE *arq;
-    arq = fopen("ListasSinais.txt","r");
+    if(rev == 1){
+        arq = fopen("ListasSinais.txt","r");
+    }
+    else if(rev == 2){
+        arq = fopen("ListasSinais2.txt","r");
+    }
     test = getc(arq);
     while(test!=EOF){
         if(test=='\n'){
@@ -167,5 +200,9 @@ int qtdSinais(){
     }
     fclose(arq);
     return cont;
+}
+
+void revisaodiaria(){
+    int canetaAzul = 0;
 }
 
